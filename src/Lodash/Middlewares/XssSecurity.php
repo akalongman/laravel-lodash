f3af17f13b4734bb30e7aec07355addafb1b55bd
Avtandil Kikabidze <akalongman@gmail.com>
2017-11-24 04:25:21 +0400

    Improve xss middleware

--- src/Lodash/Middlewares/XssSecurity.php
+++ src/Lodash/Middlewares/XssSecurity.php
@@ -18,25 +18,26 @@ class XssSecurity
 {
     public function handle(Request $request, Closure $next)
     {
+        /** @var \Illuminate\Http\Response $response */
         $response = $next($request);
 
         $request_uri = $request->getUri();
         $excluded = config('lodash.xss.exclude_uris');
         if (! empty($excluded)) {
             foreach ($excluded as $uri) {
-                if (strpos($uri, '/itdc/debug') !== false) {
+                if (strpos($request_uri, $uri) !== false) {
                     return $response;
                 }
             }
         }
 
-        // http://blogs.msdn.com/b/ieinternals/archive/2010/03/30/combating-clickjacking-with-x-frame-options.aspx
+        /** @see http://blogs.msdn.com/b/ieinternals/archive/2010/03/30/combating-clickjacking-with-x-frame-options.aspx */
         $response->headers->set('X-Frame-Options', config('lodash.xss.x_frame_options'), true);
 
-        // http://msdn.microsoft.com/en-us/library/ie/gg622941(v=vs.85).aspx
+        /** @see http://msdn.microsoft.com/en-us/library/ie/gg622941(v=vs.85).aspx */
         $response->headers->set('X-Content-Type-Options', config('lodash.xss.x_content_type_options'), true);
 
-        // http://msdn.microsoft.com/en-us/library/dd565647(v=vs.85).aspx
+        /** @see http://msdn.microsoft.com/en-us/library/dd565647(v=vs.85).aspx */
         $response->headers->set('X-XSS-Protection', config('lodash.xss.x_xss_protection'), true);
 
         return $response;

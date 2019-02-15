cdc37fdb6fa5879fb9de91d9fc70b1a3b44d7293
Avtandil Kikabidze <akalongman@gmail.com>
2019-02-15 21:49:37 +0400

    Add predis test

--- tests/Unit/RedisTest.php
+++ tests/Unit/RedisTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace Tests\Unit;
 
 use Longman\LaravelLodash\Redis\RedisManager;
+use Predis\Client;
 use Redis;
+use RedisArray;
 
 class RedisTest extends TestCase
 {
@@ -12,40 +14,94 @@ class RedisTest extends TestCase
     public function it_should_set_custom_serializer()
     {
         $redis = $this->createConnection('phpredis', [
+            'cluster' => false,
             'default' => [
-                'serializer' => 'igbinary',
+                'host'         => getenv('REDIS_HOST') ?: '127.0.0.1',
+                'port'         => getenv('REDIS_PORT') ?: 6379,
+                'database'     => 5,
+                'options'      => ['prefix' => 'lodash:'],
+                'timeout'      => 0.5,
+                'read_timeout' => 1.5,
+                'serializer'   => 'igbinary',
             ],
         ]);
+        /** @var \Redis $client */
         $client = $redis->connection()->client();
 
         $data = ['name' => 'Georgia'];
         $redis->set('country', $data, null, 60);
 
+        $this->assertInstanceOf(Redis::class, $client);
         $this->assertEquals($client->getOption(Redis::OPT_SERIALIZER), Redis::SERIALIZER_IGBINARY);
         $this->assertEquals($redis->get('country'), $data);
     }
 
-    public function createConnection(string $driver, array $config = []): RedisManager
+    /** @test */
+    public function it_should_return_predis_connection()
     {
-        $host = getenv('REDIS_HOST') ?: '127.0.0.1';
-        $port = getenv('REDIS_PORT') ?: 6379;
-        $defaultConfig = [
+        $redis = $this->createConnection('predis', [
             'cluster' => false,
             'default' => [
-                'host'         => $host,
-                'port'         => $port,
+                'host'         => getenv('REDIS_HOST') ?: '127.0.0.1',
+                'port'         => getenv('REDIS_PORT') ?: 6379,
                 'database'     => 5,
                 'options'      => ['prefix' => 'lodash:'],
                 'timeout'      => 0.5,
                 'read_timeout' => 1.5,
             ],
-        ];
-        $options = array_replace_recursive($defaultConfig, $config);
+        ]);
 
+        /** @var \Predis\Client */
+        $client = $redis->connection()->client();
+
+        $redis->set('country_predis', 'asdfg');
+
+        $this->assertInstanceOf(Client::class, $client);
+        $this->assertEquals($redis->get('country_predis'), 'asdfg');
+    }
+
+    /** @test */
+    public function it_should_set_custom_serializer_for_cluster()
+    {
+        $redis = $this->createConnection('phpredis', [
+            'clusters' => [
+                'options' => [
+                    'lazy_connect'    => true,
+                    'connect_timeout' => 1,
+                    'read_timeout'    => 3,
+                    'password'        => getenv('REDIS_PASSWORD') ?: null,
+                    'database'        => 5,
+                    'prefix'          => 'lodash:',
+                    'serializer'      => 'igbinary',
+                ],
+
+                'default' => [
+                    [
+                        'host' => getenv('REDIS_HOST') ?: '127.0.0.1',
+                        'port' => getenv('REDIS_PORT') ?: 6379,
+                    ],
+                ],
+            ],
+        ]);
+        /** @var \RedisArray $client */
+        $client = $redis->connection()->client();
+
+        $data = ['name' => 'Georgia'];
+        $redis->set('country2', $data, null, 60);
+
+        $this->assertInstanceOf(RedisArray::class, $client);
+        $this->assertEquals($redis->get('country2'), $data);
+        foreach ($client->getOption(Redis::OPT_SERIALIZER) as $serializer) {
+            $this->assertEquals($serializer, Redis::SERIALIZER_IGBINARY);
+        }
+    }
+
+    private function createConnection(string $driver, array $config = []): RedisManager
+    {
         if (version_compare($this->app->version(), '5.7', '<')) {
-            $redis = new RedisManager($driver, $options);
+            $redis = new RedisManager($driver, $config);
         } else {
-            $redis = new RedisManager($this->app, $driver, $options);
+            $redis = new RedisManager($this->app, $driver, $config);
         }
 
         return $redis;

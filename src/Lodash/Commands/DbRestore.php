9c3153873c036f7b61d1c87f2d91934253483916
Avtandil Kikabidze <akalongman@gmail.com>
2019-10-18 17:37:50 +0400

    Remove doc-block and reformat project code

--- src/Lodash/Commands/DbRestore.php
+++ src/Lodash/Commands/DbRestore.php
@@ -1,12 +1,5 @@
 <?php
-/*
- * This file is part of the Laravel Lodash package.
- *
- * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 declare(strict_types=1);
 
 namespace Longman\LaravelLodash\Commands;

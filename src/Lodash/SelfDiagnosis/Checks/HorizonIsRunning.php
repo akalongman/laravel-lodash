c8291b5850911a8fef9d899315571857971558b2
Avtandil Kikabidze <akalongman@gmail.com>
2020-03-18 15:29:55 +0400

    Fix horizon status check

--- src/Lodash/SelfDiagnosis/Checks/HorizonIsRunning.php
+++ src/Lodash/SelfDiagnosis/Checks/HorizonIsRunning.php
@@ -5,21 +5,21 @@ declare(strict_types=1);
 namespace Longman\LaravelLodash\SelfDiagnosis\Checks;
 
 use BeyondCode\SelfDiagnosis\Checks\Check;
-use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Support\Str;
-use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\Console\Output\OutputInterface;
+use Laravel\Horizon\Contracts\MasterSupervisorRepository;
 
+use function collect;
 use function trans;
 
 class HorizonIsRunning implements Check
 {
-    /** @var \Illuminate\Contracts\Console\Kernel */
-    private $kernel;
+    /**
+     * @var \Laravel\Horizon\Contracts\MasterSupervisorRepository
+     */
+    private $supervisorRepository;
 
-    public function __construct(Kernel $kernel)
+    public function __construct(MasterSupervisorRepository $supervisorRepository)
     {
-        $this->kernel = $kernel;
+        $this->supervisorRepository = $supervisorRepository;
     }
 
     public function name(array $config): string
@@ -29,18 +29,13 @@ class HorizonIsRunning implements Check
 
     public function check(array $config): bool
     {
-        $output = new BufferedOutput(
-            OutputInterface::VERBOSITY_NORMAL,
-            false,
-        );
-
-        $this->kernel->call('horizon:status', [], $output);
-        $status = $output->fetch();
-        if (Str::contains($status, 'inactive')) {
+        if (! $masters = $this->supervisorRepository->all()) {
             return false;
         }
 
-        return true;
+        return ! collect($masters)->contains(static function ($master) {
+            return $master->status === 'paused';
+        });
     }
 
     public function message(array $config): string

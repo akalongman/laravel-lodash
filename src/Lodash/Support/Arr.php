f6267c44d3322f3e4f58b70d3f116f30fc7219e4
Avtandil Kikabidze <akalongman@gmail.com>
2023-11-06 01:23:08 +0400

    Add arr helper method

--- src/Lodash/Support/Arr.php
+++ src/Lodash/Support/Arr.php
@@ -6,7 +6,14 @@ namespace Longman\LaravelLodash\Support;
 
 use Illuminate\Support\Arr as BaseArr;
 
+use function array_diff_key;
+use function array_flip;
+use function array_merge;
+
 class Arr extends BaseArr
 {
-    //
+    public static function removeKeys(array $haystack, array ...$keys): array
+    {
+        return array_diff_key($haystack, array_flip(array_merge(...$keys)));
+    }
 }

0ed7b885e7ffba3789b627fbd0efa73745b18b76
Avtandil Kikabidze <akalongman@gmail.com>
2025-03-23 14:57:26 +0400

    Improve data wrapper

--- src/Lodash/Http/Resources/ErrorResource.php
+++ src/Lodash/Http/Resources/ErrorResource.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Longman\LaravelLodash\Http\Resources;
 
-use function array_merge;
-
 class ErrorResource extends JsonResource
 {
     public static $wrap = null;
@@ -15,25 +13,5 @@ class ErrorResource extends JsonResource
         $this->resource = $resource;
 
         $this->setDataWrapper('');
-    }
-
-    public function toArray($request): array
-    {
-        /**
-         * Merge additional info and unset it, due to it causing
-         * `errors` array to be wrapped in unnecessary `data` property
-         */
-        $result = array_merge(
-            [
-                'errors' => [
-                    'general' => $this->resource,
-                ],
-            ],
-            $this->additional ?? [],
-        );
-
-        $this->additional([]);
-
-        return $result;
     }
 }

75a3be87d5af29e09ce04986271ead97ae0b515a
Avtandil Kikabidze <akalongman@gmail.com>
2026-01-12 17:42:57 +0400

    Pass transofmable to methods and allow relation getters

--- src/Lodash/Http/Resources/TransformsData.php
+++ src/Lodash/Http/Resources/TransformsData.php
@@ -9,10 +9,12 @@ use Longman\LaravelLodash\Support\Str;
 
 use function array_replace;
 use function call_user_func_array;
+use function explode;
 use function in_array;
 use function is_array;
 use function key;
 use function method_exists;
+use function str_contains;
 
 trait TransformsData
 {
@@ -37,26 +39,26 @@ trait TransformsData
      */
     protected static array $hideInOutput = [];
 
-    public static function getTransformFields(): array
+    public static function getTransformFields(?TransformableContract $model = null): array
     {
         return static::$transformMapping;
     }
 
-    public static function getInternalFields(): array
+    public static function getInternalFields(?TransformableContract $model = null): array
     {
         return static::$internalMapping;
     }
 
-    public static function getHideInOutput(): array
+    public static function getHideInOutput(?TransformableContract $model = null): array
     {
         return static::$hideInOutput;
     }
 
     public static function transformToApi(TransformableContract $model): array
     {
-        $fields = static::getTransformFields();
+        $fields = static::getTransformFields($model);
         $hiddenProperties = $model->getHidden();
-        $hideInOutput = static::getHideInOutput();
+        $hideInOutput = static::getHideInOutput($model);
         $transformed = [];
         foreach ($fields as $internalField => $transformValue) {
             if (in_array($internalField, $hiddenProperties, true)) {
@@ -75,10 +77,10 @@ trait TransformsData
         return $transformed;
     }
 
-    public static function transformToInternal(array $fields): array
+    public static function transformToInternal(array $fields, ?TransformableContract $model = null): array
     {
-        $transformFields = static::getTransformFields();
-        $transformFields = array_replace($transformFields, static::getInternalFields());
+        $transformFields = static::getTransformFields($model);
+        $transformFields = array_replace($transformFields, static::getInternalFields($model));
 
         $modelTransformedFields = [];
         foreach ($transformFields as $key => $transformField) {
@@ -99,11 +101,11 @@ trait TransformsData
         return $transformed;
     }
 
-    public static function transformCollectionToInternal(iterable $items): array
+    public static function transformCollectionToInternal(iterable $items, ?TransformableContract $model = null): array
     {
         $return = [];
         foreach ($items as $item) {
-            $return[] = static::transformToInternal($item);
+            $return[] = static::transformToInternal($item, $model);
         }
 
         return $return;
@@ -120,6 +122,26 @@ trait TransformsData
                 $value = $model->$method();
             } else {
                 throw new LogicException('Method ' . $method . ' does not available not for resource ' . static::class . ', not for model ' . $model::class);
+            }
+        } elseif (str_contains($transformValue, '.')) { // Getter contains relation
+            [$relation, $transformValue] = explode('.', $transformValue);
+            if (! $model->getRelation($relation)) {
+                throw new LogicException('Relation ' . $relation . ' does not available for model ' . $model::class.' to transform');
+            }
+
+            // Try to find getter for external field
+            $method = 'get' . Str::snakeCaseToPascalCase($transformValue);
+            if (method_exists($model->$relation, $method)) {
+                $key = $transformValue;
+                $value = $model->$relation->$method();
+            } else {
+                // Call getter for internal field
+                $method = 'get' . Str::snakeCaseToPascalCase($internalField);
+                if (! method_exists($model->$relation, $method)) {
+                    throw new LogicException('Field ' . $internalField . ' getter (' . $method . ') does not available for model ' . $model->$relation::class);
+                }
+                $key = $transformValue;
+                $value = $model->$relation->$method();
             }
         } else {
             // Try to find getter for external field
